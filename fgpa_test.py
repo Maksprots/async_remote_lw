f76478908095525d5d92d7b497a7c019bfce375f
Maksprots <makslive@bk.ru>
2023-04-09 23:55:08 +0300

    класс для загрузки с прошивкой

--- fgpa_test.py
+++ fgpa_test.py
@@ -1,9 +1,11 @@
 import subprocess
 from config import Config as cf
 
+TEST_FIRMWARE = ''
+
 
 class Plic:
-    def __init__(self, number_of_cores):
+    def __init__(self):
         self.flow = open('d', 'w')
         quartus = subprocess.run(cf.quartus_pgm_path + " -l",
                                  stdout=self.flow,
@@ -12,7 +14,7 @@ class Plic:
                                  text=True)
 
         print(quartus.stdout, "\n")
-        fpga_list = quartus.stdout.split("Info: Processing started:", 2)[0]
+        # fpga_list = quartus.stdout.split("Info: Processing started:", 2)[0]
         if quartus.stdout.find(f"{cf.plic_number}) ") != -1:
             current_port = quartus.stdout.split(f"{cf.plic_number}) ", 2)[1]
             self.current_port = current_port.split('\n', 1)[0]
@@ -21,8 +23,6 @@ class Plic:
             raise Exception('плата не нашлась')
 
     def load_to_plic(self, firmware_path):
-        # одно ядро
-
         result = subprocess.run(cf.command_to_load_firmware_1.
                                 format(cf.quartus_pgm_path,
                                        self.current_port,
@@ -32,8 +32,10 @@ class Plic:
                                 stderr=self.flow,
                                 shell=True,
                                 text=True)
-        if result.stdout.count(cf.main_key):
 
-            return "OK"
-        else:
-            return "Прошить плату не удалось"
+        return True if result.stdout.count(cf.main_key) else False
+
+
+if __name__ == '__main__':
+    c = Plic()
+    print(c.load_to_plic(TEST_FIRMWARE))
